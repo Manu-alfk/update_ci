3aebac0d49aeaa7e4d1a42703adad7ccd6e961b4
Manu <manuel1de1jesus@gmail.com>
2023-06-08 10:02:18 -0600

    segundo commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 const {autoUpdater} = require('electron-updater')
+const path = require('path')
 const log = require('electron-log');
-log.transports.file.resolvePath = () => path.join('C:\Users\HP\Desktop\programacion\electron\pruebas', 'logs/main.log');
-log.info('Hello, log');
-log.warn('Some problem appears');
+
+log.transports.file.resolvePath = () => path.join('C:/Users/HP/Desktop/programacion/electron/pruebas', '/logs/main.log');
+log.info("App version = " + app.getVersion())
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -14,13 +15,35 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
+
+autoUpdater.on('checking-for-update',()=>{
+  log.info("checking-for-update");
+})
+
+autoUpdater.on('update-available',(info) => {
+  log.info("update-available")
+})
+
+autoUpdater.on('update-not-available',(info)=>{
+  log.info("update-not-available");
+})
+
+autoUpdater.on('error',(err)=>{
+  log.info("error in auto updater "+ err);
+})
+
+autoUpdater.on('download-progress',(progress)=>{
+  log.info("progress ")
+  log.info(progress);
+})
+
+autoUpdater.on('update-downloaded',(info)=>{
+  log.info("update-downloaded")
+})
+
 app.whenReady().then(() => {
   createWindow()  
   autoUpdater.checkForUpdatesAndNotify()
 })
 
-autoUpdater.on("update-available",() => {
-  log.info("update-available")
-})
 
-//[Environment]::SetEnvironmentVariable("GH_TOKEN","token","user")
